53fbb74416cc23d40b355e5a6525df37d6007cf5
Surajgaire0 <trex1999.suraj12@gmail.com>
2022-06-01 09:38:38 +0545

    create functions for bin and hex conversion

--- week1/introduction/8.py
+++ week1/introduction/8.py
@@ -1,5 +1,19 @@
 # 8. Write a program to convert 108 to binary.
 
-a=108
+num = 108
+
+def decimal_to_binary(num):
+    result = i = 0
+    while num > 0:
+        num, rem = num // 2, num % 2
+        result += rem * (10 ** i)
+        i += 1
+    return result
+
+print (f'Binary of {num} is {decimal_to_binary(num)}')
+
+
+# Alternative solution using built-in function
+a = 108
 
 print (f'Binary of {a} is {bin(a)[2:]}')
--- week1/introduction/9.py
+++ week1/introduction/9.py
@@ -1,5 +1,20 @@
 # 9. Write a program to convert 1008 to hexadecimal.
 
-a=1008
+num = 1008
 
-print (f'Binary of {a} is {hex(a)[2:]}')
+def decimal_to_hexadecimal(num):
+    result = ''
+    replacements = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
+    while num > 0:
+        num, rem = num // 16, num % 16
+        rem = replacements[rem] if rem >= 10 else str(rem)
+        result = rem + result
+    return result
+
+print (f'Hexadecimal of {num} is {decimal_to_hexadecimal(num)}')
+
+
+# Alternative solution using built-in function
+a = 1008
+
+print (f'Hexadecimal of {a} is {hex(a)[2:]}')
